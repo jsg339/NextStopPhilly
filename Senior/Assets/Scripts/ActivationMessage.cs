e143100d2c60135e31de832c33cc14044d6c2bd8
amankartha <32320021+amankartha@users.noreply.github.com>
2021-10-13 12:37:42 -0400

    first

---
+++ Senior/Assets/Scripts/ActivationMessage.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ActivationMessage : MonoBehaviour
+{
+
+    [SerializeField] Text text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GunMessageOn()
+    {
+        text.text = "GUN";
+
+        Debug.Log("gun");
+    }
+    public void GunMessageOff()
+    {
+
+        text.text = "  ";
+        Debug.Log("dec");
+
+    }
+    public void ThumbsUpMessageOn()
+    {
+        text.text = "ThumbsUp";
+
+        Debug.Log("thumb");
+    }
+    public void ThumbsUpMessageOff()
+    {
+
+        text.text = "  ";
+
+    }
+
+    public void ChopMessageOn()
+    {
+        text.text = "Chop";
+
+        Debug.Log("Chop");
+    }
+    public void ChopMessageOff()
+    {
+
+        text.text = "  ";
+
+    }
+    public void ClapMessageOn()
+    {
+        text.text = "Clap";
+
+        Debug.Log("Clap");
+    }
+    public void ClapMessageOff()
+    {
+
+        text.text = "  ";
+
+    }
+}
